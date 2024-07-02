2d051e3ced4cd155699763c09067b26e36f72ab6
Abolhasan Ashori <abolhasan.ashori@gmail.com>
2024-07-02 20:02:15 +0330

    feat: temp commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,49 @@ import {
   Map,
   MapProps,
   Marker,
+  MarkerProps,
   Popup,
 } from '@abolhasanashori/react-neshan-map';
 import './App.css';
 import { MapStyleNameType } from '@neshan-maps-platform/mapbox-gl/dist/src/parameters/parameters';
+import { Attributes, useEffect, useState } from 'react';
 
-const defaultProps: MapProps = {
+const defaultProps = {
   mapKey: import.meta.env.VITE_MAP_KEY,
   center: [
     +import.meta.env.VITE_MAP_LONGITUDE,
     +import.meta.env.VITE_MAP_LATITUDE,
-  ] as Coordination,
+  ],
   zoom: +import.meta.env.VITE_MAP_ZOOM,
   mapType: import.meta.env.VITE_MAP_TYPE as MapStyleNameType,
-};
+} satisfies MapProps;
+
+interface MarkerInstance extends MarkerProps, Attributes {}
+
+function randomizer(num: number): number {
+  return Math.floor(Math.random() * 400) / 1e4 + num;
+}
+
+function generateMarkers(center: Coordination, count = 10): MarkerInstance[] {
+  return new Array(count).fill('').map(() => {
+    const randomed = center.map(randomizer) as Coordination;
+
+    return {
+      key: randomed.join(','),
+      lngLat: randomed,
+    };
+  });
+}
+
+const markers: MarkerInstance[] = generateMarkers(defaultProps.center);
 
 function App() {
+  const [marks, setMarks] = useState(markers);
+
+  useEffect(() => {
+    setInterval();
+  }, []);
+
   return (
     <Map
       style={{
@@ -32,11 +59,18 @@ function App() {
       traffic={false}
       {...defaultProps}
     >
-      <Marker lngLat={defaultProps.center as Coordination}>
+      {marks.map(props => (
+        <Marker {...props}>
+          <Popup closeButton={false} show>
+            {props.key as string}
+          </Popup>
+        </Marker>
+      ))}
+      {/* <Marker lngLat={defaultProps.center as Coordination}>
         <Popup closeButton={false} show>
           Hello World
         </Popup>
-      </Marker>
+      </Marker> */}
     </Map>
   );
 }
